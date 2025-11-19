c510f0998d4d2bd1ac3b66e18f6e14108b27f856
jchabrowski <chabrowski.kuba@gmail.com>
2025-11-19 14:35:45 +0100

    browser router basename

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/movie-app'>
       <QueryClientProvider client={queryClient}>
         <ErrorBoundary>
           <Routes>
